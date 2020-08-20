83ce69c34d3e62d9bd7d2497dbd8175d1be7015e
Sanny Do <sanny.do94@gmail.com>
2020-08-20 07:53:03 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -36,7 +36,7 @@ end
 
 def find_even_values(src)
   row_index = 0
-  while row_index < src.count. do
+  while row_index < src.count do
     element_index = 0
     while element_index < src[row_index].count do
         if src[row_index][element_index].even?
