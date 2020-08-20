b249de60be38ebd217851890956424842b6c91bb
Sanny Do <sanny.do94@gmail.com>
2020-08-20 07:39:40 +0000

    Automatically backed up by Learn

--- lib/iteration_with_loops.rb
+++ lib/iteration_with_loops.rb
@@ -20,7 +20,8 @@ if array_of_arrays[1][2].even?
  p array_of_arrays[1][2]
 if array_of_arrays[1][3].even?
  p array_of_arrays[1][3]
-if 
+if array_of_arrays[2][0].even?
+  p 
 
 puts find_even_values(array_of_arrays)
 end
